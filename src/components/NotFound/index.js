2ea4b2a882eb2d69782954281b6a62b1195f8b73
geethasu <nxtwave@ip-10-2-45-255.ap-south-1.compute.internal>
2024-07-06 06:16:28 +0000

    first commit

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,10 @@
+// Write your code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="notFound-bg-container">
+    <h1 className="not-found-cls">page Not Found!!</h1>
+  </div>
+)
+
+export default NotFound
